271df44074bcfcae91a51eb81eaa72cafd41d6db
Max Thom Stahl <max@dangerouscuteness.com>
2010-03-10 15:12:49 -0600

    Working solution for problem 225.

--- problem_225.hs
+++ problem_225.hs
@@ -11,5 +11,18 @@
 
 module Main where
 
-tribonaccis :: [Integer]
-tribonaccis = 1 : 1 : 1 : zipWith3 (\a b c -> a + b + c) tribonaccis (drop 1 tribonaccis) (drop 2 tribonaccis)
+tribonaccis_mod_n :: Integer -> [Integer]
+tribonaccis_mod_n n = tribonaccis 
+                      where tribonaccis = 1 : 1 : 3 : zipWith3 (\a b c -> (a + b + c) `mod` n) tribonaccis (drop 1 tribonaccis) (drop 2 tribonaccis)
+
+extract_cycle (a:b:c:cs) | a * b * c == 1 = []
+                         | otherwise = a : extract_cycle (b:c:cs)
+
+test (0:_) = False
+test (1:1:1:xs) = True
+test (_:xs) = test xs
+
+candidates = filter (test . tribonaccis_mod_n) [1,3..]
+
+main :: IO ()
+main = do print $ take 124 candidates
