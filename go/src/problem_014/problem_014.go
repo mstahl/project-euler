c9a76d0f9f1f0b2d778f622df3dd1abb31ab9fd8
max thom stahl <max@villainousindustries.com>
2013-05-27 15:16:35 -0500

    Decent but recursive solution to problem 14.

---
+++ go/src/problem_014/problem_014.go
@@ -0,0 +1,45 @@
+/*
+ * The following iterative sequence is defined for the set of positive
+ * integers:
+ * 
+ * n -> n/2 (n is even)
+ * n -> 3n + 1 (n is odd)
+ * 
+ * Using the rule above and starting with 13, we generate the following
+ * sequence:
+ * 
+ * 13 -> 40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
+ * 
+ * It can be seen that this sequence (starting at 13 and finishing at 1)
+ * contains 10 terms. Although it has not been proved yet (Collatz Problem), it
+ * is thought that all starting numbers finish at 1.
+ * 
+ * Which starting number, under one million, produces the longest chain?
+ * 
+ * NOTE: Once the chain starts the terms are allowed to go above one million.
+ */
+
+package main
+
+func collatz_length(n int) int {
+  if n == 1 {
+    return 1
+  }
+  if n & 1 == 1 {
+    return 1 + collatz_length(3 * n + 1)
+  } else {
+    return 1 + collatz_length(n >> 1)
+  }
+}
+
+func main() {
+  longest_starting_number, longest_chain_length := 0, 0
+
+  for i := 1; i < 1000000; i++ {
+    if cl := collatz_length(i); cl > longest_chain_length {
+      longest_chain_length = cl
+      longest_starting_number = i
+    }
+  }
+  println(longest_starting_number)
+}
