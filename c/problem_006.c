b9a588a7fcb3201b86d49e38fb8e46dc5d729dbf
max thom stahl <max@villainousindustries.com>
2014-10-08 12:24:56 -0500

    s/uint64/uint64_t/

--- c/problem_006.c
+++ c/problem_006.c
@@ -16,24 +16,24 @@
 
 #define MAXIMUM 100
 
-uint64 sum_of_squares_to_n(uint64 n) {
-  uint64 sum = 0;
+uint64_t sum_of_squares_to_n(uint64_t n) {
+  uint64_t sum = 0;
   // TODO There is a formula for this
-  for(uint64 i = 1; i <= n; ++i) {
+  for(uint64_t i = 1; i <= n; ++i) {
     sum += i * i;
   }
   return sum;
 }
 
-uint64 square_of_sum_to_n(uint64 n) {
-  uint64 sum = 0;
+uint64_t square_of_sum_to_n(uint64_t n) {
+  uint64_t sum = 0;
   // TODO There is a formula for this
-  for(uint64 i = 1; i <= n; ++i) {
+  for(uint64_t i = 1; i <= n; ++i) {
     sum += i;
   }
   return sum * sum;
 }
 
 int main(void) {
-  printf("%lu\n", square_of_sum_to_n(MAXIMUM) - sum_of_squares_to_n(MAXIMUM));
+  printf("%llu\n", square_of_sum_to_n(MAXIMUM) - sum_of_squares_to_n(MAXIMUM));
 }
