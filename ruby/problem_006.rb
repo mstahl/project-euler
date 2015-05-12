3795642d9b64136161e675a4395cfdb21b447df8
Max Thom Stahl <mstahl@instructure.com>
2015-05-11 18:03:33 -0700

    Starting fresh in Ruby

--- ruby/problem_006.rb
+++
@@ -1,8 +0,0 @@
-
-
-
-
-sum_of_squares = (1..100).map {|s| s * s}.inject(0) {|s, x| s + x}
-square_of_sum  = (1..100).inject(0) {|s, x| s + x} ** 2
-
-p square_of_sum - sum_of_squares
--- ruby/problem_016.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby -KU
-# 
-# 2^(15) = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
-# 
-# What is the sum of the digits of the number 2^(1000)?
-# 
-# http://projecteuler.net/index.php?section=problems&id=16
-
-puts (2 ** 1000).to_s.split(//).map {|x| x.to_i}.inject(0) {|s, x| s + x}.to_s
